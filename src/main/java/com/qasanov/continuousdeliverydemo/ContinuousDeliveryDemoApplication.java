ad7e379edf23c30179013d0f2e1eb00a04215915
Zafar Hasanov <zaferhasanov@gmail.com>
2020-04-19 11:19:30 +0400

    Initial commit

---
+++ src/main/java/com/qasanov/continuousdeliverydemo/ContinuousDeliveryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qasanov.continuousdeliverydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousDeliveryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousDeliveryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qasanov/continuousdeliverydemo/ContinuousDeliveryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qasanov.continuousdeliverydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousDeliveryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
